3d19cd6b7716733c336d58dca5d417fa24535ba4
CodrinE <ecodrin@yahoo.com>
2020-04-06 17:18:22 +0300

    added jpa querry methods and updated made Category-Recipe Entities connection

--- src/main/java/com/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/com/springframework/recipeapp/controllers/IndexController.java
@@ -1,10 +1,17 @@
 package com.springframework.recipeapp.controllers;
 
+import com.springframework.recipeapp.domain.repositories.CategoryRepository;
+import com.springframework.recipeapp.domain.repositories.UnitOfMeasureRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@AllArgsConstructor
 public class IndexController {
+
+    public CategoryRepository categoryRepository;
+    public UnitOfMeasureRepository unitOfMeasureRepository;
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
--- src/main/java/com/springframework/recipeapp/domain/repositories/CategoryRepository.java
+++ src/main/java/com/springframework/recipeapp/domain/repositories/CategoryRepository.java
@@ -1,7 +1,13 @@
 package com.springframework.recipeapp.domain.repositories;
 
 import com.springframework.recipeapp.domain.Category;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
