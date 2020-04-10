1bbe287e2481608325c23ef3a24450855f2456d4
CodrinE <ecodrin@yahoo.com>
2020-04-10 20:41:47 +0300

    added form and post method to save recipe

--- src/main/java/com/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/com/springframework/recipeapp/controllers/IndexController.java
@@ -1,10 +1,13 @@
 package com.springframework.recipeapp.controllers;
 
+import com.springframework.recipeapp.commands.RecipeCommand;
 import com.springframework.recipeapp.services.RecipeService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +22,19 @@ public class IndexController {
         log.debug("Getting Index Page...");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand saveCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + saveCommand.getId();
     }
 }
