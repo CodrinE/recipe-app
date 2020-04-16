614db29da2ac480bac582cc6e8ef7cb8da1b5d77
CodrinE <ecodrin@yahoo.com>
2020-04-16 20:19:46 +0300

    added image controller, image service, mock test and image upload form

---
+++ src/main/java/com/springframework/recipeapp/controllers/ImageController.java
@@ -0,0 +1,35 @@
+package com.springframework.recipeapp.controllers;
+
+import com.springframework.recipeapp.services.ImageService;
+import com.springframework.recipeapp.services.RecipeService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Controller
+@AllArgsConstructor
+public class ImageController {
+
+    private final ImageService imageService;
+    private final RecipeService recipeService;
+
+    @GetMapping("recipe/{recipeId}/image")
+    public String showUploadForm(@PathVariable String recipeId, Model model){
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+        return "recipe/imageuploadform";
+    }
+
+    @PostMapping("/recipe/{recipeId}/image")
+    public String handleImagePost(@PathVariable String recipeId, @RequestParam("imagefile") MultipartFile file){
+        imageService.saveImageFile(Long.valueOf(recipeId), file);
+
+        return "redirect:/recipe/" + recipeId + "/show";
+    }
+}
---
+++ src/test/java/com/springframework/recipeapp/controllers/ImageControllerTest.java
@@ -0,0 +1,66 @@
+package com.springframework.recipeapp.controllers;
+
+import com.springframework.recipeapp.commands.RecipeCommand;
+import com.springframework.recipeapp.services.ImageService;
+import com.springframework.recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class ImageControllerTest {
+
+    @Mock
+    ImageService imageService;
+
+    @Mock
+    RecipeService recipeService;
+
+    ImageController imageController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        imageController = new ImageController(imageService, recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
+    }
+
+    @Test
+    void showUploadForm() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(1L);
+
+        when(recipeService.findCommandById(anyLong())).thenReturn(command);
+
+        mockMvc.perform(get("/recipe/1/image"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+
+    @Test
+    void handleImagePost() throws Exception {
+        MockMultipartFile multipartFile =
+                new MockMultipartFile("imagefile", "test.txt",
+                        "text/plain", "Testing Spring Framework".getBytes());
+
+        mockMvc.perform(multipart("/recipe/1/image").file(multipartFile))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/recipe/1/show"));
+
+        verify(imageService, times(1)).saveImageFile(anyLong(), any());
+    }
+}
