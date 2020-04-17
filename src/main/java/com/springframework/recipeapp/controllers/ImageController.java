19834b46c41a43e4cdd03e55e32466ffe0b4edd7
CodrinE <ecodrin@yahoo.com>
2020-04-17 09:41:48 +0300

    added method to get image from db and test for it

--- src/main/java/com/springframework/recipeapp/controllers/ImageController.java
+++ src/main/java/com/springframework/recipeapp/controllers/ImageController.java
@@ -1,9 +1,11 @@
 package com.springframework.recipeapp.controllers;
 
+import com.springframework.recipeapp.commands.RecipeCommand;
 import com.springframework.recipeapp.services.ImageService;
 import com.springframework.recipeapp.services.RecipeService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static java.util.Objects.isNull;
 
 @Slf4j
 @Controller
@@ -31,5 +41,24 @@ public class ImageController {
         imageService.saveImageFile(Long.valueOf(recipeId), file);
 
         return "redirect:/recipe/" + recipeId + "/show";
+    }
+
+    @GetMapping("recipe/{id}/recipeimage")
+    public void renderDBImage(@PathVariable String id, HttpServletResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(id));
+
+        if(!isNull(recipeCommand.getImage())){
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+
+            int i = 0;
+
+            for(Byte wrappedByte : recipeCommand.getImage()){
+                byteArray[i++] = wrappedByte;
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream stream = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(stream, response.getOutputStream());
+        }
     }
 }
